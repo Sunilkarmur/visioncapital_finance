a5f26a90e1e96e20ebadc8a7c39f750b38898208
Sunil Karmur <sunil.k@lipitech.in>
2021-04-20 22:17:29 +0530

    conflic

--- resources/views/application-form-official-use.blade.php
+++ resources/views/application-form-official-use.blade.php
@@ -119,120 +119,137 @@
 @endpush
 
 @push('script')
+    <!-- BEGIN PAGE LEVEL SCRIPTS -->
     <script src="{{ asset('assets/js/jquery.validate.min.js') }}"></script>
     <script type="text/javascript">
+        $('.managment_review_select').change(function(){
+            if($(this).val()==1){
+                $('.management_review_text_row').removeClass('d-none');
+            }
+            else{
+                $('.visit_review_select').addClass('d-none');
+                $('.management_review_text_row').addClass('d-none');
+            }
+        });
+        $('.visit_select').change(function(){
+            if($(this).val()==1){
+                $('.visit_text_row').removeClass('d-none');
+                $('.visit_review_row').removeClass('d-none');
+                $('.visit_review_select').removeClass('d-none');
+                // $('.document_row').removeClass('d-none');
+            }
+            else{
+                $('.visit_review_row').addClass('d-none');
+                $('.visit_review_select').removeClass('d-none');
+                // $('.document_row').addClass('d-none');
+            }
+        });
+        $('.visit_review_select').change(function(){
+            if($(this).val()==1){
+                $('.visit_review_text_row').removeClass('d-none');
+                $('.attend_by_row').removeClass('d-none');
+                $('.document_row').removeClass('d-none');
+            }
+            else{
+                $('.visit_review_text_row').addClass('d-none');
+                $('.attend_by_row').addClass('d-none');
+                $('.document_row').addClass('d-none');
+            }
+        });
+
+        $('.document_select').change(function(){
+            if($(this).val()==1){
+                // $('.client_document_row').removeClass('d-none');
+                $('.document_review_row').removeClass('d-none');
+                $('.client_document_row').removeClass('d-none');
+                $('.document_review_text_row').removeClass('d-none');
+                $('.client_document_text_row').removeClass('d-none');
+            }
+            else{
+                // $('.gaurantor_document_text_row').addClass('d-none');
+                $('.document_review_row').addClass('d-none');
+                // $('.client_document_row').addClass('d-none');
+                $('.document_review_text_row').addClass('d-none');
+                $('.gaurantor_document_text_row').addClass('d-none');
+                $('.client_document_text_row').addClass('d-none');
+                $('.client_document_row').addClass('d-none');
+            }
+        });
+        $('.client_document_select').change(function(){
+            if($(this).val()==1){
+                $('.client_document_text_row').removeClass('d-none');
+                $('.gaurantor_document_text_row').addClass('d-none');
+            }
+            else if($(this).val()==0){
+                $('.gaurantor_document_text_row').removeClass('d-none');
+                $('.client_document_text_row').addClass('d-none');
+            }
+        });
+
+        $('.cibil_socre_required_type').change(function(){
+            if($(this).val()==1){
+                $('.cibil_score_row').removeClass('d-none');
+                $('.mgmt_review_row').removeClass('d-none');
+            }
+            else{
+                $('.cibil_score_row').addClass('d-none');
+                $('.mgmt_review_row').addClass('d-none');
+            }
+        });
+        $('.managment_review_select').change(function(){
+            if($(this).val()==1){
+                $('.cibil_score_management_review_text_row').removeClass('d-none');
+                $('.attend_by_row').removeClass('d-none');
+                $('.visit_row').removeClass('d-none');
+            }
+            else{
+                $('.cibil_score_management_review_text_row').addClass('d-none');
+                $('.attend_by_row').addClass('d-none');
+                $('.visit_row').addClass('d-none');
+            }
+        });
+
         $(document).ready(function(){
+            $("#pill-vertical-official").steps({
+                enableAllSteps: 0,
+                headerTag: "h3",
+                bodyTag: "section",
+                transitionEffect: "slideLeft",
+                cssClass: 'pills wizard',
+                titleTemplate: '#title#',
+                stepsOrientation: "vertical",
+                onStepChanging: function (event, currentIndex, newIndex) {
+                    if (currentIndex > newIndex) {
 
-            $('.managment_review_select').change(function(){
-                if($(this).val()==1){
-                    $('.management_review_text_row').removeClass('d-none');
-                }
-                else{
-                    $('.visit_review_select').addClass('d-none');
-                    $('.management_review_text_row').addClass('d-none');
-                }
-            });
-            $('.visit_select').change(function(){
-                if($(this).val()==1){
-                    $('.visit_text_row').removeClass('d-none');
-                    $('.visit_review_row').removeClass('d-none');
-                    $('.visit_review_select').removeClass('d-none');
-                    // $('.document_row').removeClass('d-none');
-                }
-                else{
-                    $('.visit_review_row').addClass('d-none');
-                    $('.visit_review_select').removeClass('d-none');
-                    // $('.document_row').addClass('d-none');
-                }
-            });
-            $('.visit_review_select').change(function(){
-                if($(this).val()==1){
-                    $('.visit_review_text_row').removeClass('d-none');
-                    $('.attend_by_row').removeClass('d-none');
-                    $('.document_row').removeClass('d-none');
-                }
-                else{
-                    $('.visit_review_text_row').addClass('d-none');
-                    $('.attend_by_row').addClass('d-none');
-                    $('.document_row').addClass('d-none');
-                }
-            });
+                        return true;
+                    }
 
-            $('.document_select').change(function(){
-                if($(this).val()==1){
-                    // $('.client_document_row').removeClass('d-none');
-                    $('.document_review_row').removeClass('d-none');
-                    $('.client_document_row').removeClass('d-none');
-                    $('.document_review_text_row').removeClass('d-none');
-                    $('.client_document_text_row').removeClass('d-none');
-                }
-                else{
-                    // $('.gaurantor_document_text_row').addClass('d-none');
-                    $('.document_review_row').addClass('d-none');
-                    // $('.client_document_row').addClass('d-none');
-                    $('.document_review_text_row').addClass('d-none');
-                    $('.gaurantor_document_text_row').addClass('d-none');
-                    $('.client_document_text_row').addClass('d-none');
-                    $('.client_document_row').addClass('d-none');
-                }
-            });
-            // $('.document_review_select').change(function(){
-            //     if($(this).val()==1){
-            //         $('.client_document_row').removeClass('d-none');
-            //         $('.document_review_text_row').removeClass('d-none');
-            //         $('.client_document_text_row').removeClass('d-none');
-            //     }
-            //     else{
-            //         $('.document_review_text_row').addClass('d-none');
-            //         $('.gaurantor_document_text_row').addClass('d-none');
-            //         $('.client_document_text_row').addClass('d-none');
-            //         $('.client_document_row').addClass('d-none');
-            //     }
-            // });
+                    var status = false;
 
+                    switch (currentIndex) {
+                        case 0:
+                            return referranceDetailForm(currentIndex);
+                        case 1:
+                            return borrowerDetailForm(currentIndex)
+                        case 2:
+                            return businessBakingForm(currentIndex)
+                        case 3:
+                            return residenceDetailForm(currentIndex);
+                        case 4:
+                            return financeBakingForm(currentIndex);
+                        case 5:
+                            return guarantorDetailsForm(currentIndex);
+                            break;
 
-            $('.client_document_select').change(function(){
-                if($(this).val()==1){
-                    $('.client_document_text_row').removeClass('d-none');
-                    $('.gaurantor_document_text_row').addClass('d-none');
-                }
-                else if($(this).val()==0){
-                    $('.gaurantor_document_text_row').removeClass('d-none');
-                    $('.client_document_text_row').addClass('d-none');
-                }
-            });
-
-            // $('.cibil_socre_checked').change(function(){
-            //     if($(this).val()==1){
-            //         $('.cibil_score_row').removeClass('d-none');
-            //         $('.mgmt_review_row').removeClass('d-none');
-            //     }
-            //     else{
-            //         $('.cibil_score_row').addClass('d-none');
-            //         $('.mgmt_review_row').addClass('d-none');
-            //     }
-            // });
-
-            $('.cibil_socre_required_type').change(function(){
-                if($(this).val()==1){
-                    $('.cibil_score_row').removeClass('d-none');
-                    $('.mgmt_review_row').removeClass('d-none');
-                }
-                else{
-                    $('.cibil_score_row').addClass('d-none');
-                    $('.mgmt_review_row').addClass('d-none');
-                }
-            });
-            $('.managment_review_select').change(function(){
-                if($(this).val()==1){
-                    $('.cibil_score_management_review_text_row').removeClass('d-none');
-                    $('.attend_by_row').removeClass('d-none');
-                    $('.visit_row').removeClass('d-none');
-                }
-                else{
-                    $('.cibil_score_management_review_text_row').addClass('d-none');
-                    $('.attend_by_row').addClass('d-none');
-                    $('.visit_row').addClass('d-none');
+                    }
+                    console.log(status)
+                    return status;
+                },
+                onFinished: function (event, currentIndex) {
+                    var status = guarantorDetailsForm(currentIndex);
+                    if (status){
+                        window.location.href=base_url+'application';
+                    }
                 }
             });
         });
@@ -243,31 +260,5 @@
     <script src="{{ asset('plugins/jquery-step/custom-jquery.steps.js') }}"></script>
     <script src="{{ asset('plugins/input-mask/jquery.inputmask.bundle.min.js') }}"></script>
     <script src="{{ asset('plugins/input-mask/input-mask.js') }}"></script>
-    <script>
-        $(document).ready(function (e) {
-            $("#pill-vertical-official").steps({
-                enableAllSteps: 0,
-                headerTag: "h3",
-                bodyTag: "section",
-                transitionEffect: "slideLeft",
-                cssClass: 'pills wizard',
-                titleTemplate: '#title#',
-                stepsOrientation: "vertical",
-                startIndex: 0,
-                onStepChanging: function (event, currentIndex, newIndex) {
-                    if (currentIndex > newIndex) {
-
-                        return true;
-                    }
-
-                    var status = true;
-                    return status;
-                },
-                onFinished: function (event, currentIndex) {
-                    return guarantorDetailsForm(currentIndex);
-                }
-            });
-        })
-    </script>
     <!-- END PAGE LEVEL SCRIPTS -->
 @endpush
